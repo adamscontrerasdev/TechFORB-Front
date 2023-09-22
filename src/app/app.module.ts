ae69d51d69910f6a196dece64ac07ce91d4c1499
adams contreras <adamscontreras18@gmail>
2023-09-22 15:06:06 -0300

    Carga final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SpaComponent } from './Components/spa/spa.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { Navbar2Component } from './components/navbar2/navbar2.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SpaComponent,
+    NavbarComponent,
+    Navbar2Component,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
